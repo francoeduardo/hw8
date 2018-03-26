92b7c90a72e13e7eb024430864602caba631c6bc
francoeduardo <35854414+francoeduardo@users.noreply.github.com>
2018-03-25 21:52:36 -0700

    Update overdue.ino

--- overdue.ino
+++ overdue.ino
@@ -1,3 +1,4 @@
+
 int ledPin = 5;
 
 /*
@@ -11,21 +12,11 @@ int ledPin = 5;
 */
 
 
-// the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ledPin, OUTPUT);
 }
 
-// the loop function runs over and over again forever
-
-/*void loop() {
-  digitalWrite(ledPin, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(100);                       // wait for a second
-  digitalWrite(ledPin, LOW);    // turn the LED off by making the voltage LOW
-  delay(100);                       // wait for a second
-}
- */
 
 void loop() {
   // fade the LED on thisPin from off to brightest:
